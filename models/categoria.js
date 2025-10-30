47b35bf60d6b01c9d2232c80c2971f72c74731a3
Lucas <paragithub01@gmail.com>
2025-10-30 19:27:50 -0300

    feat: modelo de la tabla categorias

--- models/categoria.js
+++ models/categoria.js
@@ -1,1 +1,18 @@
 // Model for Categoria
+
+import { DataTypes } from "sequelize";
+import sequelize from "../conexion/database.js";
+
+const Categoria = sequelize.define("Categoria", {
+  id_categoria: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  nombre_categoria: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+export default Categoria;
