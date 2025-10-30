84070a6f84c721a1abdf20dac1f7f74945d6579b
Lucas <paragithub01@gmail.com>
2025-10-30 19:36:46 -0300

    feat: modelo de la tabla contenido

--- models/contenido.js
+++ models/contenido.js
@@ -1,1 +1,36 @@
 // Model for Contenido
+
+import { DataTypes } from "sequelize";
+import sequelize from "../conexion/database.js";
+import Categoria from "../Categoria.js";
+
+const Contenido = sequelize.define("Contenido", {
+  id_contenido: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  titulo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  resumen: {
+    type: DataTypes.TEXT,
+  },
+  temporadas: {
+    type: DataTypes.INTEGER,
+    defaultValue: 1,
+  },
+  trailer_url: {
+    type: DataTypes.STRING,
+  },
+  id_categoria: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Categoria,
+      key: "id_categoria",
+    },
+  },
+});
+
+export default Contenido;
