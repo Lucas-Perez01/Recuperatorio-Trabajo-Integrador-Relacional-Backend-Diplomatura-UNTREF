3529e2138ceda61b5b4da65690f17fa99fcb9822
Lucas <paragithub01@gmail.com>
2025-10-30 19:37:33 -0300

    feat: modelo de la tabla genero

--- models/genero.js
+++ models/genero.js
@@ -1,1 +1,18 @@
 // Model for Genero
+
+import { DataTypes } from "sequelize";
+import sequelize from "../conexion/database.js";
+
+const Genero = sequelize.define("Genero", {
+  id_genero: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  nombre_genero: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+export default Genero;
